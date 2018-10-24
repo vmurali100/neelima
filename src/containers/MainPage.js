fac33d511f2e3997906eac3f4a2d4fc112c5f2ac
Murali Veesambattu <MVeesambattu@simeiosolutions.com>
2018-10-24 20:08:23 +0530

    second changes

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -78,7 +78,9 @@ const AccordionContent1 = () => (
                     <div className="cbox">
                         <input type="radio" className="radioBtn"/>
                     </div>
-            </div>      
+                </div> 
+
+                <button className="btn btn-success">ENROLL AND SIGN AGGREMENT</button>     
        </div>
       <div className="col-md-5">
       <h5>Your monthly cost</h5>
@@ -88,7 +90,16 @@ const AccordionContent1 = () => (
             <p className="currentBalance">$2000</p>
       <p className="creditCard">MONTHLY PAYMENT</p>
       <p className="monthlyPayment">$4.20</p>
+      <p><label>How Payments Work : </label> We will add monthly payments to your current credit card balance at your next statement cycle</p>
 
+        <h5>What you will get </h5>
+
+        <ul>
+            <li><p><label>Loss of life : </label> Cancels the remaining loan balance as of date of death(up to $70,000)</p></li>
+            <li><p><label>Disablitiy : </label> Cancels up to 12 payments per occurance (aggregate maximum of $24,000, up to $1000 per month)</p></li>
+            <li><p><label>Involunatary Unemployment : </label> Cancels up to 6 payments per occurance (aggregate maximum of $15,000, up to $1000 per month)</p></li>
+
+        </ul>
       </div>
     </div>
     </div>
